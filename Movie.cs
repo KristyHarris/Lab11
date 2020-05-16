3672610103b03ba72279ebe9d469f8f2c2030a67
Kristy <harrisk26@gmail.com>
2020-05-16 19:01:25 -0400

    First Commit

---
+++ Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    public class Movie
+    {
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+        public string Title { get; set; }
+        public string Category { get; set;  }
+
+        public virtual void GetMovieName()
+        {
+            Console.WriteLine($"{Title}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            Console.WriteLine("Enter a category: ");
+            input = Console.ReadLine().ToLower();
+
+            List<Movie> movieList = new List<Movie>
+              {
+                  new Movie("Soylent Green", "scifi"),
+                  new Movie("Star Trek II : The Wrath of Khan", "scifi"),
+                  new Movie("Shrek", "animated"),
+                  new Movie("Frozen", "animated"),
+                  new Movie("Finding Nemo", "animated"),
+                  new Movie("Moonlight", "drama"),
+                  new Movie("A Marriage Story", "drama"),
+                  new Movie("The Omen", "horror"),
+                  new Movie("Holloween", "horror"),
+                  new Movie("The Exorcist", "horror")
+                  
+              };
+
+            int index = movieList.FindIndex(item => item.Category == input);
+            if (index >= 0)
+            {
+                movieList[index].GetMovieName();
+               
+            }
+
+            //for(int i=0; i < movieList.Count; i++)
+            //{
+            //    movieList[i].GetMovieName();
+            //}
+
+            //Console.WriteLine("Welcome to the Movie List!");
+            //string input;
+
+            //do
+            //{
+            //    Console.WriteLine("Please pick from the following categories: animated, scifi, drama, horror");
+            //    input = Console.ReadLine().ToLower();
+            //} while (input != validInput);
+
+
+
+
+
+
+
+
+            //foreach(var film in movieList)
+            //{
+            //    if( string ==)
+            //}
+
+
+
+
+            //string input;
+
+            //do
+            //{
+            //Console.WriteLine("");
+            //input = Console.ReadLine();
+        }
+    }
+}
